46eb288b14f1299f07ad4e1936f7b2a0d81679ae
Reginald Chand <rschand01@gmail.com>
2024-07-19 01:44:31 +1200

    Setup cors, cookie-parser, express-session & lusca

--- server.mjs
+++ server.mjs
@@ -1,16 +1,67 @@
+import { ONE_DAY_IN_SECONDS } from "./src/constant/constant.mjs";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { config as dotenvConfig } from "dotenv";
 import express from "express";
 import { logger } from "./src/config/logger.config.mjs";
+import lusca from "lusca";
 import { mongoDataBase } from "./src/database/mongodb.database.mjs";
 import { rateLimiter } from "./src/middleware/rate.limit.middleware.mjs";
+import session from "express-session";
 
 const app = express();
 
 dotenvConfig();
 mongoDataBase();
 
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  optionsSuccessStatus: 200,
+};
+
+const cookieOptions = {
+  httpOnly: true,
+  path: "/",
+  sameSite: "strict",
+  secure: true,
+  maxAge: ONE_DAY_IN_SECONDS,
+  expires: ONE_DAY_IN_SECONDS,
+};
+
+const sessionOptions = {
+  secret: process.env.EXPRESS_SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: cookieOptions,
+};
+
+const luscaOptions = {
+  csrf: true,
+  csp: {
+    policy: {
+      "default-src": "'self'",
+      "img-src": "*",
+    },
+  },
+  xframe: "SAMEORIGIN",
+  p3p: "ABCDEF",
+  hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
+  xssProtection: true,
+  nosniff: true,
+  referrerPolicy: "same-origin",
+};
+
+if (process.env.NODE_ENV !== "production") {
+  cookieOptions.secure = false;
+}
+
 try {
   app.use(express.json());
+
+  app.use(cors(corsOptions));
+  app.use(cookieParser(process.env.COOKIE_SESSION_SECRET, cookieOptions));
+  app.use(session(sessionOptions));
+  app.use(lusca(luscaOptions));
 
   app.use(rateLimiter);
 
