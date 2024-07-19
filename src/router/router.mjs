99351a8a550a9d1caf0cf1972adc70db792391e6
Reginald Chand <hy.rschand@gmail.com>
2024-07-19 20:38:41 +1200

    Add signUpController

--- src/router/router.mjs
+++ src/router/router.mjs
@@ -1,6 +1,9 @@
 import { csrfController } from "../controller/csrf.controller.mjs";
 import express from "express";
+import { signUpController } from "../controller/signup.controller.mjs";
 
 export const router = express.Router();
 
 router.get("/csrf", csrfController);
+
+router.post("/auth/signup", signUpController);
