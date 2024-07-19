f09f12294bb6c08125db9bad32195e4f44b80ba2
Reginald Chand <rschand01@gmail.com>
2024-07-19 20:37:13 +1200

    Add ACCOUNT_VERIFICATION_EMAIL_TEMPLATE

--- src/constant/constant.mjs
+++ src/constant/constant.mjs
@@ -1,3 +1,5 @@
 export const WINDOWS_MS = 60 * 60 * 1000;
 export const API_LIMIT = 100;
 export const ONE_DAY_IN_SECONDS = 86400 * 1000;
+export const ACCOUNT_VERIFICATION_EMAIL_TEMPLATE =
+  "account.verification.email.template.pug";
