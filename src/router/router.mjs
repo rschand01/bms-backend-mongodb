6ead86e5a591516ae455fc792f4086c708bd91f4
Reginald Chand <rschand01@gmail.com>
2024-07-20 13:35:20 +1200

    Add accountVerificationController

--- src/router/router.mjs
+++ src/router/router.mjs
@@ -1,3 +1,4 @@
+import { accountVerificationController } from "../controller/account.verification.controller.mjs";
 import { csrfController } from "../controller/csrf.controller.mjs";
 import express from "express";
 import { signUpController } from "../controller/signup.controller.mjs";
@@ -7,3 +8,5 @@ export const router = express.Router();
 router.get("/csrf", csrfController);
 
 router.post("/auth/signup", signUpController);
+
+router.put("/auth/account-verification", accountVerificationController);
