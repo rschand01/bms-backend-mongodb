1cfda769ffefac22fbde4fd44e47e42d7884d544
Reginald Chand <rschand01@gmail.com>
2024-07-20 14:45:34 +1200

    Update ONE_DAY_IN_SECONDS

--- src/constant/constant.mjs
+++ src/constant/constant.mjs
@@ -1,6 +1,6 @@
 export const WINDOWS_MS = 60 * 60 * 1000;
 export const API_LIMIT = 100;
 export const ONE_HOUR_IN_SECONDS = 3600;
-export const ONE_DAY_IN_SECONDS = 86400 * 1000;
+export const ONE_DAY_IN_SECONDS = 86400;
 export const ACCOUNT_VERIFICATION_EMAIL_TEMPLATE =
   "account.verification.email.template.pug";
