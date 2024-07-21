a96bd96fdb86bb9ce903c8cb63e4b9ce575b064f
Reginald Chand <rschand01@gmail.com>
2024-07-21 19:42:57 +1200

    Add SALT & TEN_MINUTES_IN_SECONDS

--- src/constant/constant.mjs
+++ src/constant/constant.mjs
@@ -1,6 +1,8 @@
 export const WINDOWS_MS = 60 * 60 * 1000;
 export const API_LIMIT = 100;
+export const SALT = 10;
 export const ONE_HOUR_IN_SECONDS = 3600;
 export const ONE_DAY_IN_SECONDS = 86400;
+export const TEN_MINUTES_IN_SECONDS = 600;
 export const ACCOUNT_VERIFICATION_EMAIL_TEMPLATE =
   "account.verification.email.template.pug";
