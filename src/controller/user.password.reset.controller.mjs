59001bd8903a5fb0db3942299f0c12a884b8bba3
Reginald Chand <hy.rschand@gmail.com>
2024-07-21 22:25:57 +1200

    Create user.password.reset.controller.mjs

---
+++ src/controller/user.password.reset.controller.mjs
@@ -0,0 +1,46 @@
+import { SALT } from "../constant/constant.mjs";
+import { UserModel } from "../model/model.mjs";
+import bcrypt from "bcryptjs";
+import { catchErrorUtility } from "../utility/catch.error.utility.mjs";
+import jwt from "jsonwebtoken";
+import { userPasswordResetValidator } from "../validator/user.password.reset.validator.mjs";
+
+export const userPasswordResetController = async (request, response) => {
+  const { error, value } = userPasswordResetValidator.validate(request.body);
+
+  if (error) {
+    return response.status(400).json({ responseData: error.message });
+  }
+
+  const { token } = request.query;
+  const { password } = value;
+
+  try {
+    const userPayload = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    const existingUser = await UserModel.findOne({
+      email: { $eq: userPayload.userData },
+    });
+
+    if (!existingUser) {
+      return response.status(401).json({ responseData: "Unauthorized!" });
+    }
+
+    const passwordHash = await bcrypt.hash(password, SALT);
+
+    await existingUser.updateOne({ password: passwordHash });
+    return response
+      .status(200)
+      .json({ responseData: "Password reset successful!" });
+  } catch (error) {
+    if (
+      error.name === "TokenExpiredError" ||
+      error.name === "JsonWebTokenError" ||
+      error.name === "NotBeforeError"
+    ) {
+      return response.status(401).json({ responseData: error.message });
+    }
+
+    catchErrorUtility(error, response);
+  }
+};
