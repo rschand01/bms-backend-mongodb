f592c00cc1458e8ce0c1845907cdf4e1c3d47227
Reginald Chand <rschand01@gmail.com>
2024-07-23 20:20:12 +1200

    Add userLogoutController

--- src/router/router.mjs
+++ src/router/router.mjs
@@ -4,6 +4,7 @@ import express from "express";
 import { signInController } from "../controller/signin.controller.mjs";
 import { signUpController } from "../controller/signup.controller.mjs";
 import { tokenMiddleware } from "../middleware/token.middleware.mjs";
+import { userLogoutController } from "../controller/user.logout.controller.mjs";
 import { userPasswordResetController } from "../controller/user.password.reset.controller.mjs";
 import { userPasswordResetEmailController } from "../controller/user.password.reset.email.controller.mjs";
 import { userPasswordUpdateController } from "../controller/user.password.update.controller.mjs";
@@ -17,6 +18,7 @@ router.get("/csrf", csrfController);
 
 router.post("/auth/signup", signUpController);
 router.post("/auth/signin", signInController);
+router.post("/auth/logout", verifyJwtMiddleware, userLogoutController);
 router.post("/auth/password-reset-email", userPasswordResetEmailController);
 router.put(
   "/auth/account-verification",
