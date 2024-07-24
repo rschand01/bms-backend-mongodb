914c2b2a85b023394d87227cbe154694a34fdc04
Reginald Chand <hy.rschand@gmail.com>
2024-07-24 16:09:41 +1200

    Add MAX_BLOG_IMAGE_FILE_SIZE

--- src/constant/constant.mjs
+++ src/constant/constant.mjs
@@ -5,6 +5,7 @@ export const ONE_HOUR_IN_SECONDS = 3600;
 export const ONE_DAY_IN_SECONDS = 86400;
 export const TEN_MINUTES_IN_SECONDS = 600;
 export const MAX_PROFILE_IMAGE_FILE_SIZE = 2 * 1024 * 1024;
+export const MAX_BLOG_IMAGE_FILE_SIZE = 6 * 1024 * 1024;
 export const ACCOUNT_VERIFICATION_EMAIL_TEMPLATE =
   "account.verification.email.template.pug";
 export const USER_PASSWORD_RESET_EMAIL_TEMPLATE =
