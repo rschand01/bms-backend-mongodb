1d2bdfad3be7d97961cd5a6c0f17c268fa869ad3
Reginald Chand <rschand01@gmail.com>
2024-07-24 21:41:50 +1200

    Create blog.data.validator.mjs

---
+++ src/validator/blog.data.validator.mjs
@@ -0,0 +1,22 @@
+import Joi from "joi";
+
+export const blogDataValidator = Joi.object({
+  blogTitle: Joi.string()
+    .min(2)
+    .max(200)
+    .pattern(new RegExp("^[A-Z][a-z]*( [A-Z][a-z]*)*$"))
+    .required(),
+
+  blogContent: Joi.string()
+    .min(3)
+    .max(50000)
+    .pattern(new RegExp("^[A-Z][a-z]*( [A-z][a-z]*)*$"))
+    .required(),
+
+  blogImage: Joi.string(),
+  blogAuthor: Joi.string()
+    .min(2)
+    .max(100)
+    .pattern(new RegExp("^[A-Z][a-z]*( [A-Z][a-z]*)*$"))
+    .required(),
+}).unknown(true);
