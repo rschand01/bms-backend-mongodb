11385623c46094fd744aaa96e348363007a83006
Reginald Chand <hy.rschand@gmail.com>
2024-07-24 22:06:12 +1200

    Add blogCreateController

--- src/router/router.mjs
+++ src/router/router.mjs
@@ -1,11 +1,13 @@
 import { accountDeletionController } from "../controller/account.deletion.controller.mjs";
 import { accountVerificationController } from "../controller/account.verification.controller.mjs";
+import { blogCreateController } from "../controller/blog.create.controller.mjs";
 import { csrfController } from "../controller/csrf.controller.mjs";
 import express from "express";
 import { signInController } from "../controller/signin.controller.mjs";
 import { signUpController } from "../controller/signup.controller.mjs";
 import { tokenMiddleware } from "../middleware/token.middleware.mjs";
 import { userLogoutController } from "../controller/user.logout.controller.mjs";
+import { userNameParamMiddleware } from "../middleware/username.param.middleware.mjs";
 import { userPasswordResetController } from "../controller/user.password.reset.controller.mjs";
 import { userPasswordResetEmailController } from "../controller/user.password.reset.email.controller.mjs";
 import { userPasswordUpdateController } from "../controller/user.password.update.controller.mjs";
@@ -47,4 +49,11 @@ router.put(
   "/user/profile-update",
   verifyJwtMiddleware,
   userProfileUpdateController
+);
+
+router.post(
+  "/:userName/blog/create",
+  verifyJwtMiddleware,
+  userNameParamMiddleware,
+  blogCreateController
 );
