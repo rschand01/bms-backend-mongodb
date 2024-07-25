c35e6e552f95456e060c2b8e254ae5b3681b7854
Reginald Chand <hy.rschand@gmail.com>
2024-07-26 00:34:36 +1200

    Add CURRENT_PAGE, BLOGS_PER_PAGE, TWENTY_MINUTES_IN_SECONDS

--- src/constant/constant.mjs
+++ src/constant/constant.mjs
@@ -2,10 +2,13 @@ import path from "path";
 
 export const WINDOWS_MS = 60 * 60 * 1000;
 export const API_LIMIT = 100;
+export const CURRENT_PAGE = 1;
+export const BLOGS_PER_PAGE = 50;
 export const SALT = 10;
 export const ONE_HOUR_IN_SECONDS = 3600;
 export const ONE_DAY_IN_SECONDS = 86400;
 export const TEN_MINUTES_IN_SECONDS = 600;
+export const TWENTY_MINUTES_IN_SECONDS = 1200;
 export const MAX_FILE_SIZE = 6 * 1024 * 1024;
 export const MAX_BLOG_IMAGE_FILE_SIZE = 6 * 1024 * 1024;
 export const FILE_UPLOAD_DIRECTORY = path.resolve("temp/uploads");
