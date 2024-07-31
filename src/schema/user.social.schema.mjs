ab2204f51092fb5c72a079c1b4d82efd00c8a793
Reginald Chand <hy.rschand@gmail.com>
2024-08-01 00:46:27 +1200

    Add timestamps

--- src/schema/user.social.schema.mjs
+++ src/schema/user.social.schema.mjs
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 import { userConnectionSchema } from "./user.connection.schema.mjs";
 
-export const userSocialSchema = new mongoose.Schema({
-  _id: { type: mongoose.Types.ObjectId, ref: "User" },
-  followers: [userConnectionSchema],
-  followings: [userConnectionSchema],
-  followerCount: { type: Number, default: 0 },
-  followingCount: { type: Number, default: 0 },
-});
+export const userSocialSchema = new mongoose.Schema(
+  {
+    _id: { type: mongoose.Types.ObjectId, ref: "User" },
+    followers: [userConnectionSchema],
+    followings: [userConnectionSchema],
+    followerCount: { type: Number, default: 0 },
+    followingCount: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
