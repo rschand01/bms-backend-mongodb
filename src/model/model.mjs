2c03f9825299edcedf7780648cec0a90fb7f8863
Reginald Chand <hy.rschand@gmail.com>
2024-08-01 00:38:54 +1200

    Add SocialModel

--- src/model/model.mjs
+++ src/model/model.mjs
@@ -1,6 +1,8 @@
 import { blogDataSchema } from "../schema/blog.data.schema.mjs";
 import mongoose from "mongoose";
 import { signUpSchema } from "../schema/signup.schema.mjs";
+import { userSocialSchema } from "../schema/user.social.schema.mjs";
 
 export const UserModel = mongoose.model("User", signUpSchema);
 export const BlogModel = mongoose.model("Blog", blogDataSchema);
+export const SocialModel = mongoose.model("Social", userSocialSchema);
